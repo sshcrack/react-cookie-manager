5db6afcb7fd162f2c30f49a1c7e986d30ce013f8
Jay Freeman <james@hypership.dev>
2025-03-01 14:01:40 +0000

    Remove commented-out YouTube domain blocking code
    Clean up tracker utility functions by removing unused and commented-out code related to YouTube domain blocking, maintaining the existing blocking logic while improving code clarity

--- src/utils/tracker-utils.ts
+++ src/utils/tracker-utils.ts
@@ -20,22 +20,6 @@ export const getBlockedHosts = (
   }
   if (!preferences.Advertising) {
     blockedHosts.push(...trackers.categories.Advertising);
-
-    // Ensure YouTube domains are blocked when Advertising is declined
-    // This is a safeguard in case YouTube domains are miscategorized
-    // const youtubeHosts: string[] = [
-    //   // "youtube.com",
-    //   // "youtube-nocookie.com",
-    //   // "youtu.be",
-    //   // "ytimg.com",
-    //   // "googlevideo.com",
-    // ];
-
-    // youtubeHosts.forEach((host) => {
-    //   if (!blockedHosts.includes(host)) {
-    //     blockedHosts.push(host);
-    //   }
-    // });
   }
 
   return [...new Set(blockedHosts)]; // Remove duplicates
@@ -56,17 +40,6 @@ export const getBlockedKeywords = (
   const keywords = [
     ...new Set(blockedHosts.map((host) => host.replace(/\.[^.]+$/, ""))),
   ];
-
-  // Ensure YouTube keywords are included when Advertising is declined
-  // if (!preferences.Advertising) {
-  //   const youtubeKeywords = ["youtube", "ytimg", "googlevideo", "youtu"];
-
-  //   youtubeKeywords.forEach((keyword) => {
-  //     if (!keywords.includes(keyword)) {
-  //       keywords.push(keyword);
-  //     }
-  //   });
-  // }
 
   return keywords;
 };
